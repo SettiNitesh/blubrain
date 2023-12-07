0bc02184045d983ef2ce4e7bca57c4f8fca5c4b2
SettiNitesh <niteshs@byteridge.com>
2023-12-08 02:51:05 +0530

    Added APISlice , Store to intgerate API's and Manage State .

---
+++ src/utils/helper.ts
@@ -0,0 +1,5 @@
+export const GetBaseUrl = () => {
+  const API_URL = `https://blubrainmedicine.com/`;
+
+  return API_URL;
+};
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+import { GetBaseUrl } from "./helper";
+
+export { GetBaseUrl };
